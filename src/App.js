e559695ad6b163727e252e099358ff05a67dcea5
Laura Schell <laurabelenschell@gmail.com>
2021-09-12 14:09:29 -0300

    finished project [ app crashes when calling API ]

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Posts from "./components/Posts";
+import Pagination from "./components/Pagination";
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(10);
+  const [postsPerPage] = useState(10);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -18,11 +20,21 @@ function App() {
     fetchPosts();
   }, []);
 
-  console.log(posts);
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <div className="App">
       <h1>My APP</h1>
+      <Posts posts={currentPosts} loading={loading} />
+      <Pagination
+        totalPosts={posts.length}
+        postsPerPage={postsPerPage}
+        paginate={paginate}
+      />
     </div>
   );
 }
